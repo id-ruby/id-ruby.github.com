23fed5385f85029e89e9ea53005d905f7a976641
Nugroho Herucahyono <xinuc@xinuc.org>
2012-02-04 00:21:03 +0700

    search functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	
 	// Add wrapper to video iframe
 	$('iframe').wrap('<div class="video-post" />');
-
+  $('#search_results').tapir({'token': '4f2c13d23f61b03df3000052'});
 
 });
